2d0c9d872beae9eeb7ded54ac517799f5d744556
Andrew Patipaksiri <apax714@gmail.com>
2017-03-14 19:45:52 -0700

    import react and reactdom dependencies

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>React App#1</div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
