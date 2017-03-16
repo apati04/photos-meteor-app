916f6eacaa86ba9447afab535b1a426a18bb7ced
Andrew Patipaksiri <apax714@gmail.com>
2017-03-15 18:18:05 -0700

    init state into App Component

--- client/main.js
+++ client/main.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 import ImageList from './components/image_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= { images: [] };
+  }
   componentWillMount() {
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
       .then(response => console.log(response));
