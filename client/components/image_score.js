13a2979a3b8b626f73955825769936d622f9bbc8
Andrew Patipaksiri <apax714@gmail.com>
2017-03-15 18:57:07 -0700

    get percent of up votes and down votes, will need to style bar

--- client/components/image_score.js
+++ client/components/image_score.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ImageScore = (props) => {
-
-  
+  const { ups, downs } = props;
+  const upsPercent = `${100 * (ups / (ups + downs))}%`
+  const downsPercent = `${100 * (downs / (ups + downs))}%`;
   return (
     <div>
       Ups/Downs
