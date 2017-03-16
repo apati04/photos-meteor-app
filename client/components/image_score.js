69c92dbbc94205a6d1b1f6c45a240845483800dd
Andrew Patipaksiri <apax714@gmail.com>
2017-03-15 18:54:30 -0700

    template for image up votes and downvotes

---
+++ client/components/image_score.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ImageScore = (props) => {
+
+  
+  return (
+    <div>
+      Ups/Downs
+      <div className="progress">
+        <div classname="progress-bar progress-bar-success progress-bar-striped" />
+        <div className="progress-bar progress-bar-danger progress-bar-striped" />
+      </div>
+
+    </div>
+  )
+};
+
+export default ImageScore;
