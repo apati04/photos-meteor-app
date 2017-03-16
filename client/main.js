1427037963cda0d034f85da34bfe55c708ce52d0
Andrew Patipaksiri <apax714@gmail.com>
2017-03-15 17:11:22 -0700

    refactor app functional component into a class based component

--- client/main.js
+++ client/main.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
-const App = () => {
-  return (
-    <div>
+class App extends Component {
+  render() {
+    return (
+      <div>
       <ImageList />
-    </div>
-  );
+      </div>
+    )
+  }
 };
+
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+    .then(response => console.log(response));
 })
